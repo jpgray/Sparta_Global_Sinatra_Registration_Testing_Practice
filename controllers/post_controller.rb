209e4d1f96922d2834e8b9a7d6031ba208054e70
Jon Gray <jgray@spartaglobal.com>
2018-12-07 10:14:43 +0000

    Initial setup of sinatra app complete, index and registration complete page prepared for html

---
+++ controllers/post_controller.rb
@@ -0,0 +1,21 @@
+class PostController < Sinatra::Base
+
+  set :root, File.join(File.dirname(__FILE__), "..")
+  set :view, Proc.new { File.join(root, "views") }
+
+configure:development do
+  register Sinatra::Reloader
+end
+
+  get "/" do
+    @title = "Registration Page"
+    erb :'posts/index'
+  end
+
+
+  get "/registration_complete" do
+    # @img_id_src = 'posts/#{id}'
+    @title = "Registration Complete!"
+    erb :'posts/registration_complete'
+  end
+end
